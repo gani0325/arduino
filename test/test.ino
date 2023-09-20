27a10d7b352ef69c53548077ff8c5756bcb85e18
가은 이 <lge0325@bme.gachon.ac.kr>
2023-09-20 09:42:54 +0900

    ➕[Feat] LED 깜빡깜빡..

---
+++ test/test.ino
@@ -0,0 +1,21 @@
+#include <PCF8591.h>
+
+int led = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led, LOW);
+  Serial.write("OFF\n");
+  delay(1000);
+
+  digitalWrite(led, HIGH);
+  Serial.write("ON\n");
+  delay(1000);
+}
+
