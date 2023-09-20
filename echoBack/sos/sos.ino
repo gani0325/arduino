75defe01bee81e9a530332024a4f274e85e6ddd0
가은 이 <lge0325@bme.gachon.ac.kr>
2023-09-20 14:42:04 +0900

    ➕[Feat] sos 를 led로 표현하기

---
+++ echoBack/sos/sos.ino
@@ -0,0 +1,29 @@
+int led = 13;
+
+void setup() {
+    pinMode(led, OUTPUT);
+}
+
+void sos_s() {
+    for (int i = 0; i < 3; i++) {
+        digitalWrite(led, HIGH);
+        delay(200);
+        digitalWrite(led, LOW);
+        delay(200);
+    }
+}
+
+void sos_o() {
+    for (int i = 0; i < 3; i++) {
+        digitalWrite(led, HIGH);
+        delay(500);
+        digitalWrite(led, LOW);
+        delay(500);
+    }
+}
+
+void loop() {
+    sos_s();
+    sos_o();
+    sos_s();
+}
