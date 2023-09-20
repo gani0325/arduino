e48004e43affb947b52b6429bf338748d2b7c7fe
가은 이 <lge0325@bme.gachon.ac.kr>
2023-09-20 17:36:12 +0900

    ➕[Feat] 문자열로 채팅하기

--- chat/chat.ino
+++ chat/chat.ino
@@ -1,4 +1,5 @@
 #include <SoftwareSerial.h>
+#include <string.h>
 
 SoftwareSerial mySerial (3, 4); // rx : 받고 tx : 보내고
 
@@ -7,20 +8,24 @@ void setup() {
     Serial.begin(9600);
 }
 
+String str;
+
 void loop() {
     if (mySerial.available() > 0) { // 수신된 데이터 존재 여부 확인
-        byte data = mySerial.read(); // 바이트 단위로 읽기
+        
+        char data = mySerial.read(); // 바이트 단위로 읽기
+        str.concat(data);
+        delay(10);
 
-        Serial.print("gani Echo back : ");
-        Serial.write(data); // 문자 출력
-        Serial.println();
+        if (mySerial.available() == 0) {
+            Serial.println(str); // 문자 출력
+            str = "";
+        }
     }
 
     if (Serial.available() > 0) { // 수신된 데이터 존재 여부 확인
-        byte data = Serial.read(); // 바이트 단위로 읽기
+        char data = Serial.read(); // 바이트 단위로 읽기
 
-        Serial.print("gani Echo back : ");
         mySerial.write(data); // 문자 출력
-        Serial.println();
     }
 }
