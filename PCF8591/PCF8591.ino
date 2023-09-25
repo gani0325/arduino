1989c7f6bd08282aa722b4ab507f8f07083d313e
가은 이 <lge0325@bme.gachon.ac.kr>
2023-09-25 17:08:15 +0900

    ➕[Feat] 명령어 이용해서 led 밝기 조절하기

--- PCF8591/PCF8591.ino
+++ PCF8591/PCF8591.ino
@@ -1,5 +1,6 @@
 #include "Wire.h"
 #define PCF8591 (0x90 >> 1)     // 8bit 에서 7bit 로 변경 (1001 | 0000 -> 0100 | 1000 => 48)
+
 byte adcvalue0, adcvalue1, adcvalue2, adcvalue3;
 
 void setup()
@@ -14,25 +15,22 @@ void setup()
 void loop()
 {
     Wire.beginTransmission(PCF8591);
-    Wire.write(0x04);       // 먕량아
+    Wire.write(0x00);       // 명령어
     Wire.endTransmission();
-    Wire.requestFrom(PCF8591, 5);   // 주소, quantity
+    Wire.requestFrom(PCF8591, 1);   // 주소, quantity
 
     // auto 라서 첫 번째는 무의미해서 한번 더 받음
     adcvalue0=Wire.read();
-    adcvalue0=Wire.read();
-    adcvalue1=Wire.read();
-    adcvalue2=Wire.read();
-    adcvalue3=Wire.read();
 
     Serial.print(adcvalue0);
-    Serial.print(" ,");
-    Serial.print(adcvalue1);
-    Serial.print(" ,");
-    Serial.print(adcvalue2);
-    Serial.print(" ,");
-    Serial.print(adcvalue3);
     Serial.println();
-
-    delay(1000);
+    
+    for (int i = 0; i < 255; i++) {
+        Wire.beginTransmission(PCF8591);
+        Wire.write(0x40);       // 명령어
+        Wire.write(i);
+        Wire.endTransmission();
+        delay(10);
+    }
+    
 }
