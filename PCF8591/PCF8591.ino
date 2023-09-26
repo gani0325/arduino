2572a02a37f64c01d976bb8c8aca39038f892918
가은 이 <lge0325@bme.gachon.ac.kr>
2023-09-26 14:52:30 +0900

    ➕[Feat] MyPCF8591LiB을 이용해서 밝기 켜졌다가 작아졌다가 하기

--- PCF8591/PCF8591.ino
+++ PCF8591/PCF8591.ino
@@ -1,7 +1,8 @@
+#include "MyPCF8591LiB.h"
 #include "Wire.h"
-#define PCF8591 (0x90 >> 1)     // 8bit 에서 7bit 로 변경 (1001 | 0000 -> 0100 | 1000 => 48)
 
-byte adcvalue0, adcvalue1, adcvalue2, adcvalue3;
+#define PCF8591 (0x90 >> 1)
+
 
 void setup()
 {
@@ -9,28 +10,11 @@ void setup()
     Serial.begin(9600);
 }
 
-// I2C 통신을 위한 클럭 주파수를 수정
-// I2C 주변 장치에는 최소 작동 클럭 주파수가 없지만 일반적으로 100KHz가 기준
-
 void loop()
 {
     Wire.beginTransmission(PCF8591);
-    Wire.write(0x00);       // 명령어
-    Wire.endTransmission();
-    Wire.requestFrom(PCF8591, 1);   // 주소, quantity
+    uint8_t cmd = 0x47;
+    blinkPCF8594(cmd, PCF8591);
 
-    // auto 라서 첫 번째는 무의미해서 한번 더 받음
-    adcvalue0=Wire.read();
-
-    Serial.print(adcvalue0);
-    Serial.println();
-    
-    for (int i = 0; i < 255; i++) {
-        Wire.beginTransmission(PCF8591);
-        Wire.write(0x40);       // 명령어
-        Wire.write(i);
-        Wire.endTransmission();
-        delay(10);
-    }
-    
+    delay(1000);
 }
